4dff8886a2aa0c9c489806fa798b35f4899dcd16
sang hyeon, son <daseeni@gmail.com>
2024-12-22 19:35:14 +0900

    temporary

--- src/main.rs
+++ src/main.rs
@@ -5,6 +5,8 @@
 //!
 //!
 
+use std::io::Write;
+use std::path::Path;
 use std::{fmt, fs, io::Read};
 // struct BeMusicScript;
 // struct Mets;
@@ -94,6 +96,7 @@ pub fn read_bms_file(bms_path: &str, encoding_name: &str) -> Result<(), BMSReadE
             String::from(encoding_used.name()),
         ))
     } else {
+        use log::debug;
         use regex::Regex;
         let name_regex = Regex::new(r"([^/]*)\.[^/]*$").expect("wow");
         debug!("{:?}", name_regex.captures(bms_path));
@@ -110,6 +113,81 @@ pub fn read_bms_file(bms_path: &str, encoding_name: &str) -> Result<(), BMSReadE
         }
         Ok(())
     }
+}
+
+/// # 파일 행 출력
+
+pub fn print_file_row(file_path: &str) -> Result<String, BMSReadError> {
+    use log::debug;
+    use magic;
+    use sha3::Digest;
+    let file_path = Path::new(file_path);
+    // 경로에 있는 파일이 유효한지 확인
+    let target_file = match fs::File::open(file_path) {
+        Err(error_info) => {
+            debug!("std::io error {}", error_info);
+            return Err(BMSReadError::MissingFile(String::from(
+                file_path.to_str().unwrap(),
+            )));
+        }
+        Ok(read_file) => {
+            debug!("read {:#?}", read_file.metadata());
+            read_file
+        }
+    };
+    debug!("{:?}", target_file);
+
+    let file_metadata = match target_file.metadata() {
+        Err(error) => {
+            debug!("fail getting file size, {}", error);
+            return Err(BMSReadError::FailToReadFile(String::from(
+                file_path.to_str().unwrap(),
+            )));
+        }
+        Ok(metadata) => metadata,
+    };
+    let mut hasher = sha3::Sha3_256::new();
+    hasher.update(&fs::read(file_path).expect("fail to read"));
+    let ipfs_path = "";
+    let result_cid = /*if cfg!(target_os = "windows")*/ {
+        std::process::Command::new("pwsh")
+            .arg("-Command")
+            .arg(format!(
+                "{ipfs_path} add --offline --only-hash \"{}\"",
+                file_path.to_str().unwrap()
+            ))
+            .output()
+            .expect("fail to run pwsh")
+    } ;
+    let parsing_pattern = regex::Regex::new(r"added (\w*) .*").unwrap();
+    let cid = String::from_utf8(result_cid.stdout).unwrap();
+
+    let parsed_cid = parsing_pattern.captures(cid.as_str()).unwrap()[1].to_string();
+    let flags = magic::cookie::Flags::MIME_TYPE | magic::cookie::Flags::MIME_ENCODING;
+    let cookie = magic::Cookie::open(flags).unwrap();
+
+    let magic_db = ["D:/Tool/magic"].try_into().unwrap();
+    let cookie = cookie.load(&magic_db).unwrap();
+    let mime_type = cookie.file(file_path).unwrap();
+    let (mime_type, encoding) = if let Some((mime, encoding)) = mime_type.split_once("; charset=") {
+        (mime, encoding)
+    } else {
+        (mime_type.as_str(), "")
+    };
+    Ok(format!(
+        "{filename},{filesize},{mimetype},{encoding_type},SHA3-256,{hash_value},{cid}",
+        filename = file_path.file_name().unwrap().to_str().unwrap(),
+        mimetype = mime_type,
+        encoding_type = encoding,
+        filesize = file_metadata.len(),
+        hash_value = hasher
+            .finalize()
+            .to_vec()
+            .iter()
+            .map(|b| format!("{:02x}", b))
+            .collect::<String>(),
+        cid = parsed_cid
+    ))
 }
 
 #[cfg(test)]
@@ -146,4 +224,32 @@ mod tests {
         }
     }
 }
-fn main() {}
+fn main() {
+    let args: Vec<String> = std::env::args().collect();
+    println!("{:?}", args);
+
+    let current_path = Path::new(args[1].as_str());
+    let out_f_str = format!(
+        "./test_resource/result/{}.readed",
+        current_path.file_name().unwrap().to_str().unwrap()
+    );
+    let output_file_path = Path::new(out_f_str.as_str());
+    let mut out_put = match fs::File::create(&output_file_path) {
+        Ok(file) => file,
+        Err(msg) => {
+            println!("{:?}", msg);
+            return;
+        }
+    };
+    for entry in current_path.read_dir().unwrap() {
+        let file_row = match print_file_row(entry.unwrap().path().to_str().unwrap()) {
+            Ok(row) => row,
+            Err(err) => {
+                println!("{:?}", err);
+                continue;
+            }
+        };
+
+        out_put.write(format!("{}\n", file_row).as_bytes()).unwrap();
+    }
+}
